f95b1c43776acbbb9e7dbdc5b4d7237c71e893b6
Amruthakrishnan123 <amruthakrishnan936@gmail.com>
2026-04-05 12:50:33 +0530

    exp11done

--- src/LargestTwice.java
+++ src/LargestTwice.java
@@ -1,9 +1,41 @@
+import java.util.*;
+
 public class LargestTwice {
-    
-        // TODO: Read n
-        // TODO: Read array elements
-        // TODO: Find the largest element and its index
-        // TODO: Check if largest >= 2 * every other element
-        // TODO: Print index or -1
-    
+
+    public static int dominantIndex(int[] nums) {
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        int index = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > largest) {
+                secondLargest = largest;
+                largest = nums[i];
+                index = i;
+            } else if (nums[i] > secondLargest) {
+                secondLargest = nums[i];
+            }
+        }
+
+        if (largest >= 2 * secondLargest) {
+            return index;
+        } else {
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.println(dominantIndex(nums));
+
+        sc.close();
+    }
 }
